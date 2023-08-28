d11b580246e222b5573cee855e10dc404caa843b
Stephen Raj <stephen.datascientist@outlook.com>
2023-08-28 21:39:40 +0530

    Initial Commit

---
+++ src/settings/development.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'S~4Ndte_uYsrD}ey4k%Nj"[d2NXR3BSXZ\r,e9!vFKa`\Y?865`-$&#u=_Q%2<nC'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
